ac9b1ed9d38ba2c166dac86ac2aba6033a49dc7b
Stéphane Kuma <kumastephane@gmail.com>
2019-12-07 11:25:13 +0000

    Début de la mise en place de l'authentification

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,17 +3,51 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="security_login")
+     * @Route("/", name="security_login")
      */
     public function index()
     {
-        return $this->render('security/index.html.twig', [
+        return $this->render('security/login.html.twig', [
             'controller_name' => 'SecurityController',
         ]);
+    }
+
+    /**
+     * @Route("/login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+         if ($this->getUser()) {
+             return $this->redirectToRoute('employee_index');
+         }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        dd($error, $lastUsername);
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,56 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '80': '20rem',
+      },
+      customForms: theme => ({
+        default: {
+          input: {
+            borderRadius: theme('borderRadius.lg'),
+            backgroundColor: theme('colors.gray.200'),
+            '&:focus': {
+              backgroundColor: theme('colors.white'),
+              outline: 'none'
+            }
+          },
+          select: {
+            borderRadius: theme('borderRadius.lg'),
+            backgroundColor: theme('colors.gray.700'),
+            boxShadow: theme('boxShadow.default'),
+            lineHeight: theme('lineHeight.snug'),
+            borderColor: 'transparent',
+            '&:focus': {
+              boxShadow: 'none',
+              borderColor: 'none',
+            },
+            icon: '<svg fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>'
+          },
+          checkbox: {
+            width: theme('spacing.6'),
+            height: theme('spacing.6'),
+            size: '1.5em',
+            borderColor: 'transparent',
+            '&:focus': {
+              boxShadow: 'none',
+              borderColor: 'none',
+            },
+          },
+
+          radio: {
+            borderColor: 'transparent',
+            '&:focus': {
+              boxShadow: 'none',
+              borderColor: 'none',
+            },
+          }
+        }
+      })
+    }
   },
   variants: {},
-  plugins: []
+  plugins: [
+      require('@tailwindcss/custom-forms')
+  ]
 };
